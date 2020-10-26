1764c93fcb8ae8893192f7a9e833b76f54dd599f
Spencer Kasper <spencer.kasper@gmail.com>
2020-10-26 18:51:47 -0500

    * Add a simple example using dataset functions which I think I like the best so far.

---
+++ src/main/scala/SparkSQLDataset.scala
@@ -0,0 +1,22 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.col
+
+object SparkSQLDataset {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder()
+      .appName("Spark SQL basic example")
+      .config("spark.master", "local")
+      .getOrCreate()
+    spark.conf.set("spark.sql.crossJoin.enabled", true)
+    import spark.implicits._
+    val purchasesDF = spark.read.parquet("purchases.parquet")
+    val accountsDF = spark.read.parquet("accounts.parquet")
+
+    purchasesDF
+      .join(accountsDF, "accountId")
+      .where(col("name") === "Spencer Kasper")
+      .show()
+  }
+}
